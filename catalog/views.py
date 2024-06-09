0a92e735bd482100ebf83deb484d6ab02985113f
seeoow <fedorsazhin96@gmail.com>
2024-06-09 16:47:01 +0300

    add media & media tag, product detail page

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'catalog/home.html')
+
+
+def index(request):
+    products_list = Product.objects.all()
+    context = {
+        'products_list': products_list,
+        'title': 'Main page',
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def products (request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk),
+        'title': 'Product page',
+    }
+    return render(request, 'catalog/products.html', context)
+
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
